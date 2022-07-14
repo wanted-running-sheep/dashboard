32850b1ee263a99ea07bd3a9a8c685d03b1aacce
hajun2 <jpd93@naver.com>
2022-07-14 17:12:02 +0900

    💄 style: Navigation, Header 생성

---
+++ src/components/Header.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Button } from '@mui/material';
+interface Props {
+  window?: () => Window;
+}
+const navItems = ['Home', 'About', 'Contact'];
+
+const Header = (props: Props) => {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  return (
+    <Box sx={{display: 'flex'}}>
+    <AppBar component="nav">
+      <Toolbar variant="dense">
+        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ ml: 25 }}>
+          <MenuIcon />
+          </IconButton>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: '#fff' }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
+      </Toolbar>
+    </AppBar></Box>
+  )
+}
+
+export default Header;
