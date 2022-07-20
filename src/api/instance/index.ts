28ece6a73e5beb8835b473e0ad95daa36841c91c
areumsheep <areumsheep@gmail.com>
2022-07-20 16:36:45 +0900

    ✨ feat: server 추가

--- src/api/instance/index.ts
+++ src/api/instance/index.ts
@@ -1,8 +1,12 @@
 import axios from 'axios';
 import { HttpRequest } from '@/api/http/httpRequest';
 
+const baseURL =
+  process.env.REACT_APP_MODE === 'DEV'
+    ? 'http://localhost:8000'
+    : 'https://dasheep-board.herokuapp.com';
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL,
 });
 
 export const apiRequest = new HttpRequest(axiosInstance);
